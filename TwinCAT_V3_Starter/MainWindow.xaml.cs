3f65050079abcc33e9f5330fabebf44649154e2e
Kurt Linder <32621408+KurtLinder@users.noreply.github.com>
2018-01-23 10:55:43 +0100

    Tuple.Item1..3 direkt eingesetzt

--- TwinCAT_V3_Starter/MainWindow.xaml.cs
+++ TwinCAT_V3_Starter/MainWindow.xaml.cs
@@ -34,6 +34,7 @@ namespace TwinCAT_V3_Starter
             }
             catch (Exception exp)
             {
+                Console.WriteLine("{0} Exception 1 caught.", exp);
             }
         }
     }
@@ -69,7 +70,7 @@ namespace TwinCAT_V3_Starter
             * _AWL_ oder _AS_ oder _FUP_ oder _KOP_ oder _SCL_ oder _ST_
             * 
             * */
-            
+
             ButtonListe.Add(ProjektStarten_BUG);
             ButtonListe.Add(ProjektStarten_PLC);
             ButtonListe.Add(ProjektStarten_PLC_NC);
@@ -80,7 +81,7 @@ namespace TwinCAT_V3_Starter
             List<Tuple<string, string, string>> TupleList_PLC_VISU = new List<Tuple<string, string, string>>();
             List<Tuple<string, string, string>> TupleList_PLC_NC = new List<Tuple<string, string, string>>();
             List<Tuple<string, string, string>> TupleList_BUG = new List<Tuple<string, string, string>>();
-            
+
             System.IO.DirectoryInfo ParentDirectory = new System.IO.DirectoryInfo("Projekte");
 
             foreach (System.IO.DirectoryInfo d in ParentDirectory.GetDirectories())
@@ -167,19 +168,15 @@ namespace TwinCAT_V3_Starter
         {
             foreach (Tuple<string, string, string> Projekt in Projekte)
             {
-                string Bezeichnung = Projekt.Item1;
-                string Sprache = Projekt.Item2;
-                string Ordner = Projekt.Item3;
-
                 RadioButton rdo = new RadioButton();
-                rdo.GroupName = "TIA_PORTAL_V14_SP1";
+                rdo.GroupName = "TwinCAT V3";
                 rdo.VerticalAlignment = VerticalAlignment.Top;
                 rdo.Checked += new RoutedEventHandler(radioButton_Checked);
                 rdo.FontSize = 14;
 
                 // nur PLC und sonst nichts
-                rdo.Content = Bezeichnung + " (" + Sprache + ")";
-                rdo.Name = Ordner;
+                rdo.Content = Projekt.Item1 + " (" + Projekt.Item2 + ")";
+                rdo.Name = Projekt.Item3;
                 StackPanel.Children.Add(rdo);
             }
         }
@@ -225,20 +222,49 @@ namespace TwinCAT_V3_Starter
             System.IO.DirectoryInfo ParentDirectory = new System.IO.DirectoryInfo("Projekte");
             string sourceDirectory = ParentDirectory.FullName + "\\" + ProjektName;
 
-            DarstellungAendernListe(ButtonListe, true, Colors.Yellow, "Ordner " + ProjektPfad + " löschen");
-            if (System.IO.Directory.Exists(ProjektPfad)) System.IO.Directory.Delete(ProjektPfad, true);
+            try
+            {
+                DarstellungAendernListe(ButtonListe, true, Colors.Yellow, "Ordner " + ProjektPfad + " löschen");
+                if (System.IO.Directory.Exists(ProjektPfad)) System.IO.Directory.Delete(ProjektPfad, true);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine("{0} Exception 2 caught.", exp);
+            }
 
-            DarstellungAendernListe(ButtonListe, true, Colors.Yellow, "Ordner " + ProjektPfad + " erstellen");
-            System.IO.Directory.CreateDirectory(ProjektPfad);
+            try
+            {
+                DarstellungAendernListe(ButtonListe, true, Colors.Yellow, "Ordner " + ProjektPfad + " erstellen");
+                System.IO.Directory.CreateDirectory(ProjektPfad);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine("{0} Exception 3 caught.", exp);
+            }
 
-            DarstellungAendernListe(ButtonListe, true, Colors.Yellow, "Alle Dateien kopieren");
-            Copy(sourceDirectory, ProjektPfad);
+            try
+            {
+                DarstellungAendernListe(ButtonListe, true, Colors.Yellow, "Alle Dateien kopieren");
+                Copy(sourceDirectory, ProjektPfad);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine("{0} Exception 4 caught.", exp);
+            }
 
-            DarstellungAendernListe(ButtonListe, true, Colors.LawnGreen, "Projekt mit TwinCAT V3 öffnen");
-            Process proc = new Process();
-            proc.StartInfo.FileName = ProjektPfad + "\\start.cmd";
-            proc.StartInfo.WorkingDirectory = ProjektPfad;
-            proc.Start();
+            try
+            {
+                DarstellungAendernListe(ButtonListe, true, Colors.LawnGreen, "Projekt mit TwinCAT V3 öffnen");
+                Process proc = new Process();
+                proc.StartInfo.FileName = ProjektPfad + "\\start.cmd";
+                proc.StartInfo.WorkingDirectory = ProjektPfad;
+                proc.Start();
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine("{0} Exception 5 caught.", exp);
+            }
+
         }
 
 
