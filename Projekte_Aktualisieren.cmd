1b45abe6af7f5bf397af5d4d44530a7d2927faf5
Kurt Linder <32621408+KurtLinder@users.noreply.github.com>
2018-02-21 11:52:58 +0100

    Projekte löschen/aktualisieren

---
+++ Projekte_Aktualisieren.cmd
@@ -0,0 +1,3 @@
+rd "c:\git\TwinCAT-V3-Starter\TwinCAT_V3_Starter\bin\Debug\Projekte\" /S /Q
+xcopy "s:\Linder Kurt\TIA Portal V14 SP1\Projekte\*.*"  "c:\git\TwinCAT-V3-Starter\TwinCAT_V3_Starter\bin\Debug\Projekte\" /k/h/e/v/c/q/r 
+pause
---
+++ Projekte_Loeschen.cmd
@@ -0,0 +1,2 @@
+rd "c:\git\TwinCAT-V3-Starter\TwinCAT_V3_Starter\bin\Debug\Projekte\" /S /Q
+pause
